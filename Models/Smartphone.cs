177175f15f9dc20de4d772c3f6000bcf1db57f60
sebastião oliveira trindade <sebatrindade@hotmail.com>
2022-10-27 15:32:50 -0300

    Add project files.

---
+++ Models/Smartphone.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCelular.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        private string Modelo { get; set; }
+        private int Imei { get; set; }
+        private int Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, int imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo Ligação...");
+        }
+
+        public void MostrarNumero()
+        {
+            Console.WriteLine(this.Numero);
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using ProjetoCelular.Models;
+
+// Realizando os testes
+
+Smartphone nokia = new Nokia("112233445","N201",345634567,32);
+Smartphone iphone = new Iphone("987654321","13",444444444,64);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("cobrinha");
+nokia.MostrarNumero();
+nokia.Numero = "112233445";
+nokia.MostrarNumero();
+
+iphone.InstalarAplicativo("Instagram");
+iphone.ReceberLigacao();
+
